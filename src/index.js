eeeaed1538c351ea2a57a0bc0960058e41c474de
chichi5454 <140154706+chichi5454@users.noreply.github.com>
2023-08-01 13:09:06 +0300

    added relevant icon from API

--- src/index.js
+++ src/index.js
@@ -23,21 +23,30 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", `response.data.weather[0].description`);
 }
 
 let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Nairobi%20&appid=cabdbda40038ba7d1165b953b1c7bd6c&units=metric`;
+let city = "Nairobi";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cabdbda40038ba7d1165b953b1c7bd6c&units=metric`;
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
